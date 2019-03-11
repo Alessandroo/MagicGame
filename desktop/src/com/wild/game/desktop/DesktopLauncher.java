7e472caf8a89ea2c58142bb50860ebee7fbd6f30
Александр <vojtseh2014@gmail.com>
2019-03-12 00:09:07 +0300

    Initial commit

---
+++ desktop/src/com/wild/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wild.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wild.game.MagicGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MagicGame(), config);
+	}
+}
